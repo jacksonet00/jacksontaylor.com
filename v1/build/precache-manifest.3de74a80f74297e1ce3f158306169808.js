614c5b0305ffa7e11e505176e72b72b16a497239
jacksonet00 <jacksoneliastaylor@gmail.com>
2021-04-01 12:50:20 -0400

    moved v1 code

---
+++ v1/build/precache-manifest.3de74a80f74297e1ce3f158306169808.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89fd59423cc1f707e57f375c57effcf6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12c15551ad8e7350dec8",
+    "url": "/static/css/main.f3a88eaa.chunk.css"
+  },
+  {
+    "revision": "2266b20d494daa24704d",
+    "url": "/static/js/2.28a4fe36.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/static/js/2.28a4fe36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12c15551ad8e7350dec8",
+    "url": "/static/js/main.81c651b4.chunk.js"
+  },
+  {
+    "revision": "1ed064540ff84a19063d",
+    "url": "/static/js/runtime-main.f5ac08cf.js"
+  },
+  {
+    "revision": "1367104697fa19c02f926762fca681a7",
+    "url": "/static/media/ucf_logo.13671046.png"
+  }
+]);
