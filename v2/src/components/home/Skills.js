542602c64928bdaee170935fffe4a426fc7cdbbc
Jackson Taylor <jacksoneliastaylor@gmail.com>
2022-02-15 11:25:10 -0500

    update skills section

--- v2/src/components/home/Skills.js
+++ v2/src/components/home/Skills.js
@@ -1,53 +1,53 @@
-import { Box, VStack, Heading, Text, Divider, Flex } from '@chakra-ui/react';
-import { HOME_PAGE_DATA as data } from '../../constants';
+import { Box, VStack, Heading, Text, Divider, Flex } from "@chakra-ui/react";
+import { HOME_PAGE_DATA as data } from "../../constants";
 
 export const Skills = () => {
-	return (
-		<Flex
-			direction={{ lg: 'row', base: 'column' }}
-			w="xl"
-			align={{ base: 'start', lg: 'top' }}
-			justify={{ lg: 'center' }}
-		>
-			{data.skills.map((skillSet) => {
-				return (
-					<VStack
-						key={skillSet.type}
-						spacing={6}
-						paddingBottom={{ base: 6 }}
-						align={{ base: 'start', lg: 'center' }}
-						margin={{ lg: '0 auto' }}
-					>
-						<Box>
-							<Heading
-								fontSize="md"
-								paddingBottom={2}
-								textAlign={{
-									base: 'left',
-									lg: 'center',
-								}}
-							>
-								{skillSet.type}
-							</Heading>
-							<Divider w={36} />
-						</Box>
-						{skillSet.items.map((item) => {
-							return (
-								<Text
-									key={item}
-									fontSize="md"
-									textAlign={{
-										base: 'left',
-										lg: 'center',
-									}}
-								>
-									{item}
-								</Text>
-							);
-						})}
-					</VStack>
-				);
-			})}
-		</Flex>
-	);
+  return (
+    <Flex
+      direction={{ lg: "row", base: "column" }}
+      w="xl"
+      align={{ base: "start", lg: "top" }}
+      justify={{ lg: "center" }}
+    >
+      {data.skills.map((skillSet) => {
+        return (
+          <VStack
+            key={skillSet.type}
+            spacing={6}
+            paddingBottom={{ base: 6 }}
+            align={{ base: "start" }}
+            margin={{ lg: "0 auto" }}
+          >
+            <Box>
+              <Heading
+                fontSize="md"
+                // paddingBottom={2}
+                textAlign={{
+                  base: "left",
+                  lg: "center",
+                }}
+              >
+                {skillSet.type}
+              </Heading>
+              {/* <Divider w={36} /> */}
+            </Box>
+            {skillSet.items.map((item) => {
+              return (
+                <Text
+                  key={item}
+                  fontSize="md"
+                  textAlign={{
+                    base: "left",
+                    lg: "center",
+                  }}
+                >
+                  {item}
+                </Text>
+              );
+            })}
+          </VStack>
+        );
+      })}
+    </Flex>
+  );
 };
