83ca84f348f4e5e62b4fb15cf2c376e390d541a3
Jackson Taylor <jacksoneliastaylor@gmail.com>
2022-03-25 12:37:11 -0400

    move v2 to top

--- v1/src/config/analytics.js
+++
@@ -1,7 +0,0 @@
-import firebase from 'firebase';
-import firebaseConfig from './config';
-
-firebase.initializeApp(firebaseConfig);
-const analytics = firebase.analytics();
-
-export default analytics;
