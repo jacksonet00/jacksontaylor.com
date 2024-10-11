d6cdc00b0afeb23850a9be55ae100056cdbab62b
Jackson Taylor <jacksoneliastaylor@gmail.com>
2024-10-10 21:16:47 -0400

    add prodbuild script

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 function Index() {
   return (
-    <div className="flex flex-col items-center justify-center">
+    <div className="flex flex-col items-center justify-center min-h-screen">
       Hello, peasants.
     </div>
   );
