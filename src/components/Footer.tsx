77bbd8dc56837a5ec7078fbda84106da80e7501b
Jackson Taylor <jacksoneliastaylor@gmail.com>
2024-10-10 20:36:47 -0400

    kill old code

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import { Box, Text } from '@chakra-ui/layout';
-
-export const Footer = () => {
-	return (
-		<Box position="absolute" bottom={0} right={0} padding={6}>
-			<Text fontSize="xl">🧘🏼‍♂️</Text>
-		</Box>
-	);
-};
